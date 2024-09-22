da7acc533cac375bad7a7778a76403fc68b49ffc
Aleksei Churkin <kweall@mail.ru>
2024-09-22 23:46:11 +0300

    [GOJHW-2]: merge master

--- commands/give.go
+++ commands/give.go
@@ -7,13 +7,13 @@ import (
 
 func Give(storage Storage, parts []string) (err error) {
 
-	if len(parts) < 2 {
+	if len(parts) < 1 {
 		return fmt.Errorf("should be at least 1 argument: list of orderID's (int) separated by space")
 	}
 
 	// Преобразуем список orderID в массив int64
 	var orderIDs []int64
-	for _, part := range parts[1:] {
+	for _, part := range parts[0:] {
 		orderID, err := strconv.ParseInt(part, 10, 64)
 		if err != nil {
 			return fmt.Errorf("orderID is incorrect")
